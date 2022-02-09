01c7f99fe353a390f7172ecdcf648c9e7edda8cd
wpanas <8766331+wpanas@users.noreply.github.com>
2022-02-09 20:16:43 +0100

    Add tests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 use rocket::serde::{Deserialize, Serialize};
 use rocket::serde::json::Json;
@@ -9,19 +10,19 @@ fn hello(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
-#[derive(Deserialize)]
-struct Request<'a> {
-    name: &'a str
+#[derive(Serialize, Deserialize)]
+struct GreetingsRequest<'a> {
+    name: &'a str,
 }
 
-#[derive(Serialize)]
-struct Response {
-    greetings: String
+#[derive(Serialize, Deserialize, Debug)]
+struct GreetingsResponse {
+    greetings: String,
 }
 
-#[post("/", data="<content>")]
-fn greetings(content: Json<Request<'_>>) -> Json<Response> {
-    Json(Response {
+#[post("/", data = "<content>")]
+fn greetings(content: Json<GreetingsRequest<'_>>) -> Json<GreetingsResponse> {
+    Json(GreetingsResponse {
         greetings: format!("Hello {}", content.name)
     })
 }
@@ -31,4 +32,34 @@ fn rocket() -> _ {
     rocket::build()
         .mount("/hello", routes![hello])
         .mount("/greetings", routes![greetings])
+}
+
+mod tests {
+    use rocket::local::blocking::Client;
+    use crate::{GreetingsResponse, GreetingsRequest};
+
+    #[test]
+    fn hello_get() {
+        let client = Client::tracked(super::rocket())
+            .expect("valid `Rocket`");
+
+        let response = client.get("/hello/Mike").dispatch();
+        assert_eq!(response.into_string().unwrap(), "Hello, Mike!");
+    }
+
+    #[test]
+    fn greetings_post() {
+        let client = Client::tracked(super::rocket())
+            .expect("valid `Rocket`");
+
+        let response = client.post("/greetings")
+            .json(&GreetingsRequest {
+                name: "Mike"
+            }).dispatch();
+
+        let greetings_response = response
+            .into_json::<GreetingsResponse>().unwrap();
+
+        assert_eq!(greetings_response.greetings, "Hello Mike");
+    }
 }
